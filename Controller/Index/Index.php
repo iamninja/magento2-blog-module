a761b449843d5a199e0171f7fb21ec4c10205c3f
Vagios Vlachos <bagios234@gmail.com>
2015-12-25 16:39:03 +0200

    Create controller and route

---
+++ Controller/Index/Index.php
@@ -0,0 +1,26 @@
+<?php
+namespace Brokenspacebars\Blog\Controller\Index;
+
+use \Magento\Framework\App\Action\Action;
+
+/**
+ *
+ */
+class Index extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    )
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
