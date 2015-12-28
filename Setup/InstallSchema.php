f1724bd0d9dfd819036543aa36fde03372795b5b
Vagios Vlachos <bagios234@gmail.com>
2015-12-28 21:22:36 +0200

    Fix

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -1,23 +1,23 @@
-<?php
-namespace Brokenspacebars\Blog\Setup;
+<?php namespace Brokenspacebars\Blog\Setup;
 
 use Magento\Framework\Setup\InstallSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
-use Magento\Framwork\DB\Dbl\Table;
+use Magento\Framework\DB\Ddl\Table;
 
-/**
- *
- */
-class InstallSchema extends InstallSchemaInterface
+class InstallSchema implements InstallSchemaInterface
 {
-
-    function isntall(
-        SchemaSetupInterface $setup,
-        ModuleContextInterface $context
-    )
+    /**
+     * Installs DB schema for a module
+     *
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
         $installer = $setup;
+
         $installer->startSetup();
 
         $table = $installer->getConnection()
@@ -29,51 +29,18 @@ class InstallSchema extends InstallSchemaInterface
                 ['identity' => true, 'nullable' => false, 'primary' => true],
                 'Post ID'
             )
-            ->addColumn(
-                'url_key',
-                Table::TYPE_TEXT,
-                100,
-                ['nullable' => true, 'default' => null]
-            )
-            ->addColumn(
-                'title',
-                Table::TYPE_TEXT,
-                255,
-                ['nullable' => false],
-                'Blog Title'
-            )
-            ->addColumn(
-                'content',
-                Table::TYPE_TEXT,
-                '2M',
-                [],
-                'Blog Content'
-            )
-            ->addColumn(
-                'is_active',
-                Table::TYPE_SMALLINT,
-                null,
-                ['nullable' => false, 'default' => '1'],
-                'Is Post Active?'
-            )
-            ->addColumn(
-                'creation_time',
-                Table::TYPE_DATETIME,
-                null,
-                ['nullable' => false],
-                'Creation Time'
-            )
-            ->addColumn(
-                'update_time',
-                Table::TYPE_DATETIME,
-                null,
-                ['nullable' => false],
-                'Update Time'
-            )
+            ->addColumn('url_key', Table::TYPE_TEXT, 100, ['nullable' => true, 'default' => null])
+            ->addColumn('title', Table::TYPE_TEXT, 255, ['nullable' => false], 'Blog Title')
+            ->addColumn('content', Table::TYPE_TEXT, '2M', [], 'Blog Content')
+            ->addColumn('is_active', Table::TYPE_SMALLINT, null, ['nullable' => false, 'default' => '1'], 'Is Post Active?')
+            ->addColumn('creation_time', Table::TYPE_DATETIME, null, ['nullable' => false], 'Creation Time')
+            ->addColumn('update_time', Table::TYPE_DATETIME, null, ['nullable' => false], 'Update Time')
             ->addIndex($installer->getIdxName('blog_post', ['url_key']), ['url_key'])
             ->setComment('Blog Posts');
 
-            $installer->getConnection()->createTable($table);
-            $isntaller->endSetup();
+        $installer->getConnection()->createTable($table);
+
+        $installer->endSetup();
     }
+
 }
